c2445e438c5c8cbb72608212d58841b0375f8ca5
shahriariit <shahriar.setu@gmail.com>
2018-06-16 21:40:45 +0600

    update

--- upregulatedsimilarity.R
+++ upregulatedsimilarity.R
@@ -2,10 +2,6 @@ f8 <- function(x,y) {
   t(x)[t(x) %in% t(y)]
 }
 
-f8(myupuni1,myupuni4)
-f8(myupuni1,myupuni5)
-f8(myupuni1,myupuni6)
-f8(myupuni1,myupuni7)
 
 f8(myupuni4,myupuni5)
 f8(myupuni4,myupuni6)
