a7fa8a8f8b87a59c9abfe6755b0882887a69c05a
shahriariit <shahriar.setu@gmail.com>
2018-06-16 21:40:45 +0600

    update

--- upregulategene.R
+++ upregulategene.R
@@ -6,10 +6,22 @@ f5 <- function(x,y){
   write.table(x, file = y, sep = ",", col.names = NA, qmethod = "double")
 }
 
-myupuni1=f4(mylist1$Parkinson$Gene.symbol)
-myupuni2=f4(mylist1$Autism$Gene.symbol)
-myupuni3=f4(mylist1$Alzhemier$Gene.symbol)
-myupuni4=f4(mylist1$Huntington$Gene.symbol)
-myupuni5=f4(mylist1$Chronicfatigue$Gene.symbol)
-myupuni6=f4(mylist1$ALS$Gene.symbol)
-myupuni7=f4(mylist1$DM2$Gene.symbol)
+# myupuni1=f4(mylist1$Parkinson$Gene.symbol)
+# myupuni2=f4(mylist1$Autism$Gene.symbol)
+# myupuni3=f4(mylist1$Alzhemier$Gene.symbol)
+# myupuni4=f4(mylist1$Huntington$Gene.symbol)
+# myupuni5=f4(mylist1$Chronicfatigue$Gene.symbol)
+# myupuni6=f4(mylist1$ALS$Gene.symbol)
+# myupuni7=f4(mylist1$DM2$Gene.symbol)
+
+upr_str= list(mylist1$`Parkinson GSE-54536`$Gene.symbol,
+              mylist1$`Alzheimer's - GSE28146`$Gene.symbol,
+              mylist1$Huntington$Gene.symbol,
+              mylist1$`Lou Gehrig's - GSE833`$Gene.symbol,
+              mylist1$`Cerebral Palsy - GSE31243`$Gene.symbol,
+              mylist1$`Developmental Delay - GSE32012`$Gene.symbol,
+              mylist1$`Cardiomyopathy - GSE67492`$Gene.symbol,
+              mylist1$`Muscular Dystrophy - GSE29775`$Gene.symbol,
+              mylist1$`Mitochondrial Dys- GSE13887`$Gene.symbol)
+
+myupunit=lapply(upr_str,f4)
